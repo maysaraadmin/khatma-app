181ae913fed45eedffd3b7f2ce1bf41754ea160c
maysaraadmin <maysara.mubarak.mohamed@gmail.com>
2025-05-08 22:04:25 +0400

    up

---
+++ core/simple_auth.py
@@ -0,0 +1,50 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.views import View
+import logging
+
+logger = logging.getLogger(__name__)
+
+class SimpleUserCreationForm(UserCreationForm):
+    """A simpler user creation form without Profile model dependencies"""
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=30, required=False)
+    last_name = forms.CharField(max_length=30, required=False)
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        
+        if commit:
+            user.save()
+        
+        return user
+
+class SimpleRegisterView(View):
+    """A simple registration view without Profile model dependencies"""
+    template_name = 'registration/simple_register.html'
+    form_class = SimpleUserCreationForm
+    
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+    
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            logger.info(f"New user registered: {user.username}")
+            return redirect('core:index')
+        
+        logger.warning(f"Registration form invalid: {form.errors}")
+        return render(request, self.template_name, {'form': form})
